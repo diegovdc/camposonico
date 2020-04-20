36c5666d6ee9d42eeb20aa172f849e2476a4bb8c
Diego <diego.vid.eco@gmail.com>
2020-04-19 23:51:50 -0500

    Deals with empty responses

--- src/algoradio/freesound.cljs
+++ src/algoradio/freesound.cljs
@@ -33,7 +33,6 @@
                   (js/Number (get params "page")))}))
 
 (defn get-audios! [app-state query]
-;;; TODO notify user when there are more audios to load from a given query
   (let [query- (if (empty? query) "all" query)
         query* (-> query
                    (str/split " ")
@@ -48,14 +47,16 @@
                          (-> res
                              algoradio.axios/response->data
                              (get-results-and-next-page))]
-                     (js/console.log results)
-                     (swap! app-state
-                            assoc-in
-                            [:freesounds-pages query-]
-                            next-page)
-                     (swap! app-state
-                            update-in
-                            [:freesounds query-]
-                            concat results))))
+                     (if-not (empty? results)
+                       (do (swap! app-state
+                                  assoc-in
+                                  [:freesounds-pages query-]
+                                  next-page)
+                           (swap! app-state
+                                  update-in
+                                  [:freesounds query-]
+                                  concat results))
+                       (js/alert (str "No se encontraron resultados para: "
+                                      query))))))
           (.then js/console.log )
           (.catch js/console.log)))))
